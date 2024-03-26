de4310af6041a90efc0da120b88196e9a323ffb8
SEYFULLAH01 <120310366+SEYFULLAH01@users.noreply.github.com>
2024-03-26 14:03:13 +0300

    Bilgi_Yarismasi
    Web Form Udemy  Kurs Projesi

---
+++ Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bilgi_Yarismasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int soruNo = 0, dogru = 0, yanlis = 0;
+
+        private void BtnB_Click(object sender, EventArgs e)
+        {
+            BtnA.Enabled = false;
+            BtnB.Enabled = false;
+            BtnC.Enabled = false;
+            BtnD.Enabled = false;
+            BtnSonraki.Enabled = true;
+
+            label5.Text = BtnB.Text;
+            if (label4.Text == label5.Text)
+            {
+                dogru++;
+                LblDogru.Text = dogru.ToString();
+                pictureBox1.Visible = true;
+            }
+            else
+            {
+                yanlis++;
+                LblYanlis.Text = yanlis.ToString();
+                pictureBox2.Visible = true;
+            }
+        }
+
+        private void BtnC_Click(object sender, EventArgs e)
+        {
+            BtnA.Enabled = false;
+            BtnB.Enabled = false;
+            BtnC.Enabled = false;
+            BtnD.Enabled = false;
+            BtnSonraki.Enabled = true;
+
+            label5.Text = BtnC.Text;
+            if (label4.Text == label5.Text)
+            {
+                dogru++;
+                LblDogru.Text = dogru.ToString();
+                pictureBox1.Visible = true;
+            }
+            else
+            {
+                yanlis++;
+                LblYanlis.Text = yanlis.ToString();
+                pictureBox2.Visible = false;
+            }
+        }
+
+        private void BtnD_Click(object sender, EventArgs e)
+        {
+            BtnA.Enabled = false;
+            BtnB.Enabled = false;
+            BtnC.Enabled = false;
+            BtnD.Enabled = false;
+            BtnSonraki.Enabled = true;
+
+            label5.Text = BtnD.Text;
+            if (label4.Text == label5.Text)
+            {
+                dogru++;
+                LblDogru.Text = dogru.ToString();
+                pictureBox1.Visible = true;
+            }
+            else
+            {
+                yanlis++;
+                LblYanlis.Text = yanlis.ToString();
+                pictureBox2.Visible = false;
+            }
+        }
+
+        private void BtnA_Click(object sender, EventArgs e)
+        {
+            BtnA.Enabled = false;
+            BtnB.Enabled = false;
+            BtnC.Enabled = false;
+            BtnD.Enabled = false;
+            BtnSonraki.Enabled = true;
+
+            label5.Text = BtnA.Text;
+            if (label4.Text == label5.Text)
+            {
+                dogru++;
+                LblDogru.Text = dogru.ToString();
+                pictureBox1.Visible = true;
+            }
+            else
+            {
+                yanlis++;
+                LblYanlis.Text = yanlis.ToString();
+                pictureBox2.Visible = false;
+            }
+
+        }
+
+        private void BtnSonraki_Click(object sender, EventArgs e)
+        {
+            BtnA.Enabled = true;
+            BtnB.Enabled = true;
+            BtnC.Enabled = true;
+            BtnD.Enabled = true;
+            BtnSonraki.Enabled = false;
+
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+
+            soruNo++;
+            LblSoruNo.Text = soruNo.ToString();
+
+            if (soruNo == 1)
+            {
+                richTextBox1.Text = "Cumhuriyet kaç yılında ilan edilmiştir ?";
+                BtnA.Text = "1920";
+                BtnB.Text = "1922";
+                BtnC.Text = "1930";
+                BtnD.Text = "1923";
+                label4.Text = "1923";
+            }
+            if (soruNo == 2)
+            {
+                richTextBox1.Text = "Hangi şehir Ege bölgemizde yer almaz ?";
+                BtnA.Text = "Manisa";
+                BtnB.Text = "Uşak";
+                BtnC.Text = "Muğla";
+                BtnD.Text = "Yalova";
+                label4.Text = "Yalova";
+                BtnSonraki.Text = "Sonuçlar: ";
+            }
+            if (soruNo == 3)
+            {
+                BtnA.Enabled = false;
+                BtnB.Enabled = false;
+                BtnC.Enabled = false;
+                BtnD.Enabled = false;
+                BtnSonraki.Enabled = false;
+                MessageBox.Show("Doğru: " + dogru + "/n" + "Yanlış: " + yanlis);
+            }
+        }
+    }
+}
